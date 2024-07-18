7d9664c5f8b239541ff4940bbf85d26db22c4940
Raymond Chen <ahrchen@gmail.com>
2024-07-18 14:57:51 -0700

    Step 2: Create the flashcard UI

--- Flashcard/ContentView.swift
+++ Flashcard/ContentView.swift
@@ -9,13 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "dog")
-                .imageScale(.large)
-                .foregroundStyle(.pink)
-            Text("Hello, dogs!")
+        ZStack {
+            // Card background
+               RoundedRectangle(cornerRadius: 25.0)
+                .fill(Color.blue.gradient)
+                .shadow(color: .black, radius: 4, x: -2, y: 2)
+            
+            // Card text
+            VStack(spacing: 20) {
+                Text("Question")
+                    .bold()
+                
+                Rectangle()
+                    .frame(height: 1)
+                
+                Text("Located at the southern end of Puget Sound, what is the capitol of Washington?")
+                    
+            }
+            .font(.title)
+            .foregroundStyle(.white)
+            .padding()
         }
-        .padding()
+        .frame(width: 300, height: 500)
+        
+        
     }
 }
 
