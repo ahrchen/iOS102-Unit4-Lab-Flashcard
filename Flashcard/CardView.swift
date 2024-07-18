fa11776bf85dda4bf7630607a633244861f6962e
Raymond Chen <ahrchen@gmail.com>
2024-07-18 15:42:49 -0700

    Step 5: Swipe cards

--- Flashcard/CardView.swift
+++ Flashcard/CardView.swift
@@ -9,17 +9,25 @@ import SwiftUI
 
 struct CardView: View {
     
+    private let swipeThreshold: Double = 200 // <--- Define a swipeThreshold constant top level
     @State private var isShowingQuestion = true
-    
+    @State private var offset: CGSize = .zero // <-- A state property to store the offset
+
     let card: Card
     
     var body: some View {
         ZStack {
-            // Card background
-               RoundedRectangle(cornerRadius: 25.0)
-                .fill(isShowingQuestion ? Color.blue.gradient : Color.indigo.gradient)
-                .shadow(color: .black, radius: 4, x: -2, y: 2)
-            
+            ZStack {
+                // Back-most card background
+                    RoundedRectangle(cornerRadius: 25.0) // <-- Add another card background behind the original
+                        .fill(offset.width < 0 ? .red : .green) // <-- Set fill based on offset (swipe left vs right)
+                
+                // Front-most card background (i.e. original background)
+                RoundedRectangle(cornerRadius: 25.0)
+                    .fill(isShowingQuestion ? Color.blue.gradient : Color.indigo.gradient)
+                    .shadow(color: .black, radius: 4, x: -2, y: 2)
+                    .opacity(1 - abs(offset.width) / swipeThreshold)
+            }
             // Card text
             VStack(spacing: 20) {
                 Text(isShowingQuestion ? "Question" : "Answer")
@@ -39,6 +47,16 @@ struct CardView: View {
         .onTapGesture {
             isShowingQuestion.toggle()
         }
+        .opacity(3 - abs(offset.width) / swipeThreshold * 3)
+        .rotationEffect(.degrees(offset.width / 20.0))
+        .offset(CGSize(width:offset.width, height: 0))
+        .gesture(DragGesture()
+            .onChanged { gesture in // <-- onChanged called for every gesture value change, like when the drag translation changes
+                let translation = gesture.translation // <-- Get the current translation value of the gesture. (CGSize with width and height)
+                print(translation) // <-- Print the translation value
+                offset = translation
+            }
+        )
         
     }
 }
--- Flashcard/ContentView.swift
+++ Flashcard/ContentView.swift
@@ -10,17 +10,15 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var cards: [Card] = Card.mockedCards
-    
+        
     var body: some View {
         // Card deck
-       ZStack {
-           ForEach(0..<cards.count, id: \.self) { index in
-               CardView(card: cards[index])
-                   .rotationEffect(.degrees(Double(cards.count - 1 - index) * -5))
-           }
-       }
-        
-        
+        ZStack {
+            ForEach(0..<cards.count, id: \.self) { index in
+                CardView(card: cards[index])
+                    .rotationEffect(.degrees(Double(cards.count - 1 - index) * -5))
+            }
+        }
     }
 }
 
