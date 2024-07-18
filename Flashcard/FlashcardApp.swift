470baaa7b00d640c139445712f907ecbe19704f1
Raymond Chen <ahrchen@gmail.com>
2024-07-18 14:26:11 -0700

    Initial Commit

---
+++ Flashcard/FlashcardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardApp.swift
+//  Flashcard
+//
+//  Created by Raymond Chen on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
